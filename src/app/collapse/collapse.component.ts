2a3b3997b0298af9ea7884755af4d83701ad4a91
CoderGuy614 <jlutz.110@gmail.com>
2020-11-25 18:55:53 -0500

    Add Properties and Methods to collapse

--- src/app/collapse/collapse.component.ts
+++ src/app/collapse/collapse.component.ts
@@ -5,5 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './collapse.component.html'
 })
 export class CollapseComponent {
+
+  //Properties
+
+  firstName = 'Jonny';
+  lastName = 'Lutz';
   public isCollapsed = false;
+
+  //Methods
+  constructor() {
+    console.log('HELLO FROM COLLAPSE COMPONENT')
+    this.sayHello()
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`)
+  }
+
+
 }
