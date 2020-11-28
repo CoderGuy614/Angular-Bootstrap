e7a5b9260f53181f17e858db9eb5e95019ca4d33
CoderGuy614 <jlutz.110@gmail.com>
2020-11-27 20:47:55 -0500

    Add NgClass to Template

--- src/app/components/collapse/collapse.component.ts
+++ src/app/components/collapse/collapse.component.ts
@@ -11,6 +11,7 @@ export class CollapseComponent implements OnInit {
   users: User[];
   loaded: boolean = false;
   enableAdd: boolean = true;
+  currentClasses: {};
   public isCollapsed: false;
 
   //Methods
@@ -24,25 +25,35 @@ export class CollapseComponent implements OnInit {
     setTimeout(() => this.loaded = true, 2000)
   }
 
+  setCurrentClasses() {
+    this.currentClasses = {
+      'btn-success': this.enableAdd
+    }
+  }
+
   ngOnInit() {
     this.users = [
       {
         firstName: "Linda",
         lastName: 'Lutz',
         age: 55,
-        image: "http://lorempixel.com/600/600/people/3"
+        image: "http://lorempixel.com/600/600/people/3",
+        isActive: false
       },
       {
         firstName: "Jim",
         lastName: 'Lutz',
         age: 60,
-        image: "http://lorempixel.com/600/600/people/2"
+        image: "http://lorempixel.com/600/600/people/2",
+        isActive: true
+        
       },
       {
         firstName: "Jonny",
         lastName: 'Lutz',
         age: 35,
-        image: "http://lorempixel.com/600/600/people/1"
+        image: "http://lorempixel.com/600/600/people/1",
+        isActive: true
       }
     ]
     this.addUser({
@@ -52,6 +63,7 @@ export class CollapseComponent implements OnInit {
     })
  
     this.loadUsers()
+    this.setCurrentClasses()
   }
 }
 
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -2,7 +2,7 @@ export interface User {
     firstName: string,
       lastName: string, 
       age: number,
-      image?: string
-
+      image?: string,
+      isActive?: boolean
   }
   
