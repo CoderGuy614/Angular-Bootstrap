c433ead9edcab89aa592038ab93b45c8bd4e5edb
CoderGuy614 <jlutz.110@gmail.com>
2020-11-27 20:38:36 -0500

    Property Binding

--- src/app/components/collapse/collapse.component.ts
+++ src/app/components/collapse/collapse.component.ts
@@ -10,6 +10,7 @@ export class CollapseComponent implements OnInit {
   //Properties
   users: User[];
   loaded: boolean = false;
+  enableAdd: boolean = true;
   public isCollapsed: false;
 
   //Methods
@@ -28,17 +29,20 @@ export class CollapseComponent implements OnInit {
       {
         firstName: "Linda",
         lastName: 'Lutz',
-        age: 55
+        age: 55,
+        image: "http://lorempixel.com/600/600/people/3"
       },
       {
         firstName: "Jim",
         lastName: 'Lutz',
-        age: 60
+        age: 60,
+        image: "http://lorempixel.com/600/600/people/2"
       },
       {
         firstName: "Jonny",
         lastName: 'Lutz',
-        age: 35
+        age: 35,
+        image: "http://lorempixel.com/600/600/people/1"
       }
     ]
     this.addUser({
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -2,6 +2,7 @@ export interface User {
     firstName: string,
       lastName: string, 
       age: number,
+      image?: string
 
   }
   
